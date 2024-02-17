a1abbc0004649f09a739efc9a125eaeb9f072538
Rian Darmawan <riandarmawan785@gmail.com>
2024-02-17 21:05:54 +0700

    Testing Usecase

--- mock/controller_mock/auth_controller_mock.go
+++ mock/controller_mock/auth_controller_mock.go
@@ -1,7 +1,22 @@
 package controllermock
 
-import "github.com/stretchr/testify/mock"
+import (
+	"4crypto/model/dto"
+	"4crypto/model/entity"
+
+	"github.com/stretchr/testify/mock"
+)
 
 type AuthControllerMock struct {
 	mock.Mock
+}
+
+func (a *AuthControllerMock) Register(payload entity.User) (entity.User, error) {
+	args := a.Called(payload)
+	return args.Get(0).(entity.User), args.Error(1)
+}
+
+func (a *AuthControllerMock) Login(payload dto.AuthRequestDto) (dto.AuthResponseDto, error) {
+	args := a.Called(payload)
+	return args.Get(0).(dto.AuthResponseDto), args.Error(1)
 }
