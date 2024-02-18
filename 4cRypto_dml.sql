e8e728302513b6f6d733f95cd147e004309519c6
Rayanda Wiyoga Belva <rayanda.belva99@gmail.com>
2024-02-18 22:32:57 +0700

    [Dokumentasi] ddl, dml, dokumentasi menggunakan README.md

---
+++ 4cRypto_dml.sql
@@ -0,0 +1,16 @@
+-- Users
+INSERT INTO public.users
+(id, "name", email, username, "password", "role", created_at, updated_at)
+VALUES('df29b765-7235-40e3-abe7-43ebcfda6e2a'::uuid, 'Destry Faradila', 'destry.faradila@gmail.com', 'destry.faradila', '$2a$10$AuwgzIrYaZDKQHfo4WA4x.BtNMUxU/gCa9qdfozDHPdqOavqfV5Vm', 'employee', '2023-11-07 13:17:31.791', '2023-11-07 13:17:31.776');
+INSERT INTO public.users
+(id, "name", email, username, "password", "role", created_at, updated_at)
+VALUES('fc5812b9-60e0-4d1d-9130-6f9d7e8cf99c'::uuid, 'Dinda Aditiya', 'dinda.aditiya@gmail.com', 'dinda.aditiya', '$2a$10$4StptFVnCmbgoPrclxN7ROQSHgZdLBVgt8Q.L3L0Gw4sdNHvSoigq', 'employee', '2023-11-07 13:23:23.175', '2023-11-07 13:23:23.161');
+INSERT INTO public.users
+(id, "name", email, username, "password", "role", created_at, updated_at)
+VALUES('33d765b2-7170-45f2-9f3e-8794a6360531'::uuid, 'Dinda Aditiya', 'dinda.aditiya@gmail.com', 'dinda.aditiya', '$2a$10$CSycnwsGb8VdBRg.BS.84OJ8.5GeCXWtfXELXESK6Q70Ur4xx7HMS', 'employee', '2023-11-07 14:43:25.155', '2023-11-07 14:43:25.137');
+INSERT INTO public.users
+(id, "name", email, username, "password", "role", created_at, updated_at)
+VALUES('adfef570-8c30-43e0-bf5d-3a34ab908fcf'::uuid, 'Jution Candra Kirana', 'jutionck@gmail.com', 'jutionck', '$2a$10$4StptFVnCmbgoPrclxN7ROQSHgZdLBVgt8Q.L3L0Gw4sdNHvSoigq', 'admin', '2023-11-06 13:53:36.324', '2023-11-06 13:53:36.324');
+INSERT INTO public.users
+(id, "name", email, username, "password", "role", created_at, updated_at)
+VALUES('7e8097d8-fef1-49ed-9f7b-cecc9520cca8'::uuid, 'Tiara Agisti', 'tiara.agisti@gmail.com', 'tiara.agisti', '$2a$10$4StptFVnCmbgoPrclxN7ROQSHgZdLBVgt8Q.L3L0Gw4sdNHvSoigq', 'employee', '2023-11-06 13:53:36.324', '2023-11-06 13:53:36.324');
---
+++ README.md
@@ -0,0 +1,11 @@
+# Aplikasi 4cRypto
+
+## FITUR
+Fitur yang tersedia di dalam 4cRypto
+1. Testing
+2. Authorization
+3. Logger
+4. Login
+
+## Cara Menjalankan Aplikasi
+
