17c1739e78d5625a2afd309615fdf4872417e8a2
Rayanda Wiyoga Belva <rayanda.belva99@gmail.com>
2024-02-20 19:43:33 +0700

    [FIX] Fixing unmarshal

--- rest_api/cmcrank.go
+++ rest_api/cmcrank.go
@@ -1,48 +1,37 @@
-package restapi
+package main
 
-// import (
-// 	"4crypto/model/entity"
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
+import (
+	"4crypto/model/dto/res"
+	"encoding/json"
+	"fmt"
+	"log"
 
-// 	"github.com/go-resty/resty/v2"
-// )
+	"github.com/go-resty/resty/v2"
+)
 
-// func CmcRanking() {
+func main() {
 
-// 	urlAPI := "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?start=1&limit=10&sort=cmc_rank"
-// 	client := resty.New()
+	urlAPI := "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?start=1&limit=10&sort=cmc_rank"
+	client := resty.New()
 
-// 	client.SetHeader("X-CMC_PRO_API_KEY", "ba777d1f-caee-4be5-8314-335bb1c9ea35")
+	client.SetHeader("X-CMC_PRO_API_KEY", "ba777d1f-caee-4be5-8314-335bb1c9ea35")
 
-// 	resp, err := client.R().Get(urlAPI)
-// 	if err != nil {
-// 		log.Println("resp.err:", err.Error())
-// 		return
-// 	}
+	resp, err := client.R().Get(urlAPI)
+	if err != nil {
+		log.Println("resp.err:", err.Error())
+		return
+	}
 
-// 	// Kita masukkan response JSON kedalam struct TODO
-// 	var cmcRank entity.CmcRank
-// 	//convert JSON -> struct = Unmurshal
-// 	err = json.Unmarshal(resp.Body(), &cmcRank)
+	// convert JSON -> struct
+	err = json.Unmarshal(resp.Body(), &res.Response)
+	if err != nil {
+		log.Println("Unmarshal.err:", err.Error())
+		return
+	}
 
-// 	if err != nil {
-// 		log.Println("Unmarshal.err:", err.Error())
-// 		return
-// 	}
-
-// 	// // Kita masukkan status response JSON kedalam struct CmcRankStatus
-// 	// var cmcRankStatus entity.CmcRankStatus
-// 	// // convert JSON -> struct = Unmarshal
-// 	// err = json.Unmarshal(resp.Body(), &cmcRankStatus)
-// 	// if err != nil {
-// 	// 	log.Println("Unmarshal.err:", err.Error())
-// 	// 	return
-// 	// }
-
-// 	// fmt.Println("status: ")
-// 	// fmt.Println("CmcRankStatus:", cmcRankStatus)
-// 	fmt.Println("data: ")
-// 	fmt.Println("CmcRank:", string(resp.Body()))
-// }
+	fmt.Println("Status:", res.Response.Status)
+	fmt.Println("Data:")
+	for _, rank := range res.Response.Data {
+		fmt.Println(rank)
+	}
+}
